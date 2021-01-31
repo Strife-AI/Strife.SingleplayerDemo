294114b83b0ee6c49c4a2685a77f811ceaee5a45
Brendan Mulcahy <mulcahy.software@gmail.com>
2021-01-31 12:31:55 -0500

    Add batch norm, refactor to loops, remove gradient clipping

--- src/GameML/DDQN.hpp
+++ src/GameML/DDQN.hpp
@@ -38,9 +38,11 @@ struct Transition : StrifeML::ISerializable
 struct DeepQNetwork : StrifeML::NeuralNetwork<InitialState, Transition, 1>
 {
 	float discount = 0.999f; // gamma
+	const static int LayerCount = 4;
 	
     torch::nn::Embedding embedding{ nullptr };
-    torch::nn::Conv2d conv1{ nullptr }, conv2{ nullptr }, conv3{ nullptr }, conv4{ nullptr };
+    torch::nn::Conv2d conv[LayerCount]{ {nullptr}, {nullptr}, {nullptr}, {nullptr} };
+    torch::nn::BatchNorm2d batchNorm[LayerCount]{ {nullptr}, {nullptr}, {nullptr}, {nullptr} };
     torch::nn::Linear dense{ nullptr };
     std::shared_ptr<torch::optim::Adam> optimizer;
 	
@@ -55,14 +57,16 @@ struct DeepQNetwork : StrifeML::NeuralNetwork<InitialState, Transition, 1>
     	
         embedding = register_module("embedding", torch::nn::Embedding(totalObservables, embeddingSize));
 
-    	// todo brendan we can write a loop for these
-    	// todo brendan add back in batch norm
-        conv1 = register_module("conv1", torch::nn::Conv2d(4, 8, 3));
-        conv2 = register_module("conv2", torch::nn::Conv2d(8, 16, 3));
-        conv3 = register_module("conv3", torch::nn::Conv2d(16, 32, 3));
-        conv4 = register_module("conv4", torch::nn::Conv2d(32, 64, 3));
-        dense = register_module("dense", torch::nn::Linear(64, totalActions));
-        optimizer = std::make_shared<torch::optim::Adam>(parameters(), 1e-3);
+    	int convChannels = embeddingSize;
+        for (int i = 0; i < LayerCount; ++i)
+        {
+	        conv[i] = register_module("conv" + std::to_string(i), torch::nn::Conv2d(convChannels, convChannels * 2, 3));
+	        batchNorm[i] = register_module("batchNorm" + std::to_string(i), torch::nn::BatchNorm2d(convChannels * 2));
+        	convChannels *= 2;
+        }
+    	
+        dense = register_module("dense", torch::nn::Linear(convChannels, totalActions));
+        optimizer = std::make_shared<torch::optim::Adam>(parameters(), 1e-5);
     }
 
 	// todo brendan can we move this function back to the trainer?
@@ -84,13 +88,18 @@ struct DeepQNetwork : StrifeML::NeuralNetwork<InitialState, Transition, 1>
     	torch::Tensor nextValues = std::get<0>(targetNetwork->Forward(nextStates).max(1));
         torch::Tensor expectedValues = (nextValues * discount) + rewards;
 
-        std::cout << rewards << std::endl;
+        //std::cout << rewards << std::endl;
         //std::cout << expectedValues.sizes() << std::endl;
 
         torch::Tensor loss = torch::nn::functional::smooth_l1_loss(currentValues.squeeze(), expectedValues);
 
     	optimizer->zero_grad();
         loss.backward();
+
+   // 	for(auto param : parameters()) {
+			//param.mutable_grad() = param.grad().data().clamp_(-1, 1);
+   //     }
+    	
         optimizer->step();
  	
         outResult.loss = loss.item<float>();
@@ -127,23 +136,35 @@ struct DeepQNetwork : StrifeML::NeuralNetwork<InitialState, Transition, 1>
             x = squeeze(spatialInput, 1);
         }
 
-        x = embedding->forward(x);     // N x 80 x 80 x 4
-        x = x.permute({ 0, 3, 1, 2 }); // N x 4 x 80 x 80
-        
-        x = leaky_relu(conv1->forward(x)); // N x 8 x 76 x 76
-        x = dropout(x, 0.5, is_training());
-        x = max_pool2d(x, { 2, 2 }); // N x 8 x 38 x 38
+        x = embedding->forward(x);
+        x = x.permute({ 0, 3, 1, 2 });
 
-        x = leaky_relu(conv2->forward(x)); // N x 16 x 36 x 36
-        x = dropout(x, 0.5, is_training());
-        x = max_pool2d(x, { 2, 2 }); // N x 16 x 18 x 18
+        try
+        {
+	        for (int i = 0; i < LayerCount; ++i)
+	        {
+	        	bool lastLayer = i == LayerCount - 1;
+		        x = conv[i]->forward(x);
+	        	
+	            if (!lastLayer)
+	            {
+		            x = batchNorm[i]->forward(x);
+	            }
+		        
+		        x = leaky_relu(x);
+		        x = dropout(x, 0.5, is_training());
 
-        x = leaky_relu(conv3->forward(x)); // N x 32 x 16 x 16
-        x = dropout(x, 0.5, is_training());
-        x = max_pool2d(x, { 2, 2 }); // N x 32 x 8 x 8
-
-        x = leaky_relu(conv4->forward(x)); // N x 64 x 6 x 6
-        x = dropout(x, 0.5, is_training());
+	            if (!lastLayer)
+	            {
+			        x = max_pool2d(x, {2, 2});
+		        }
+	        }
+        }
+        catch (std::exception e)
+        {
+	        std::cout << e.what() << std::endl;
+        	throw;
+        }
 
         if (sequenceLength > 1)
         {
